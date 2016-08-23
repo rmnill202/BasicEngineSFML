61447884459a8693aa15e9010f2ae678970b82f1
Robert Nill <rmnill202mail@gmail.com>
2016-08-23 08:59:48 -0400

    v1.0.1  -Added Example_01 folder, in the process of updating example files'  -Compiled the engine files as a static library, if you'd like to avoid using the engine files directly in your project and recompiling them each time

---
+++ BasicEngineSFML/Example_01/main.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "BasicEngineSFML\Engine.h"
+#include "BasicEngineSFML\ResourceManager.h"
+#include "BasicEngineSFML\StateManager.h"
+#include "BasicEngineSFML\State.h"
+#include "BasicEngineSFML\Utilities.h"
+
+int main();
